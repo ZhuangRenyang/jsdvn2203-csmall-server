0323f6e8fe1b577b31fa72a7b88969f2405fe544
ZhuangRenyang <996491835@qq.com>
2022-10-21 14:42:34 +0800

    1.1

--- src/main/java/cn/tedu/jsdvn2203/csmall/server/mapper/AlbumMapper.java
+++ src/main/java/cn/tedu/jsdvn2203/csmall/server/mapper/AlbumMapper.java
@@ -2,10 +2,13 @@ package cn.tedu.jsdvn2203.csmall.server.mapper;
 
 import cn.tedu.jsdvn2203.csmall.server.pojo.entity.Album;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface AlbumMapper {
     int insert(Album album);
 
-    void delete(Album album);
+    int deleteById(Long id);
+
+    int updateNameById(@Param("id") Long id,@Param("name") String name);
 }
